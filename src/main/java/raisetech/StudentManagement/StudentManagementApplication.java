c490763599a7ee1855f4ec10cb7dcdc995ee6ed8
hata <rxbpj5367@gmail.com>
2026-07-24 04:23:27 +0900

    MyBatisでCRUD（取得・登録・更新・削除）機能を実装

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -2,11 +2,15 @@ package raisetech.StudentManagement;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 @SpringBootApplication
 @RestController
@@ -14,13 +18,16 @@ public class StudentManagementApplication {
 
 	//private String name = "tanaka tarou";
 	//private String age = "37";
+	@Autowired
+	private StudentRepository repository;
 
-	private Map<String, String> students = new HashMap<>();
 
-			public StudentManagementApplication(){
-				students.put("tanaka tarou","37");
-				students.put("sato hanako","25");
-			}
+//	private Map<String, String> students = new HashMap<>();
+//
+//			public StudentManagementApplication(){
+//				students.put("tanaka tarou","37");
+//				students.put("sato hanako","25");
+//			}
 
 
 
@@ -29,14 +36,15 @@ public class StudentManagementApplication {
 
 		//public class Student {
 			//private String name;
-			//private String age;
+			//private int age;
 
 		//};
 	}
 
-	@GetMapping("/studentInfo")
-	public Map<String, String> getStudentInfo() {
-		return students;
+	@GetMapping("/student")
+	public String getStudentInfo(@RequestParam("name") String name) {
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
 
 	//@PostMapping("/studentInfo")
@@ -44,10 +52,19 @@ public class StudentManagementApplication {
 		//return students;
 	//}
 
-	@PostMapping("/studentName")
-	public String setStudentInfo(String name, String age) {
-		students.put(name, age);
+	@PostMapping("/student")
+	public String registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 		return name + "(" + age + "歳）を追加しました";
+	}
 
+	@PatchMapping("/student")
+	public void updateStudentName(String name, int age) {
+		repository.updateStudent(name, age);
+	}
+
+	@DeleteMapping("/student")
+	public void deleteStudent(String name) {
+		repository.deleteStudent(name);
 	}
 }
