e1ce4d6e6103f64d02390fe14c09bf21070ce983
hata <rxbpj5367@gmail.com>
2026-08-06 19:25:46 +0900

    students_coursesの全件検索機能を追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,16 +1,11 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 
 @SpringBootApplication
 @RestController
@@ -42,9 +37,13 @@ public class StudentManagementApplication {
 	}
 
 	@GetMapping("/student")
-	public String getStudentInfo(@RequestParam("name") String name) {
-		Student student = repository.searchByName(name);
-		return student.getName() + " " + student.getAge() + "歳";
+	public List<Student> getStudentList() {
+		return repository.searchStudent();
+	}
+
+	@GetMapping("/studentCoursesList")
+	public List<StudentCourses> getStudentCoursesList() {
+		return repository.searchStudentCourses();
 	}
 
 	//@PostMapping("/studentInfo")
@@ -52,19 +51,19 @@ public class StudentManagementApplication {
 		//return students;
 	//}
 
-	@PostMapping("/student")
-	public String registerStudent(String name, int age) {
-		repository.registerStudent(name, age);
-		return name + "(" + age + "歳）を追加しました";
-	}
-
-	@PatchMapping("/student")
-	public void updateStudentName(String name, int age) {
-		repository.updateStudent(name, age);
-	}
-
-	@DeleteMapping("/student")
-	public void deleteStudent(String name) {
-		repository.deleteStudent(name);
-	}
+//	@PostMapping("/student")
+//	public String registerStudent(String name, int age) {
+//		repository.registerStudent(name, age);
+//		return name + "(" + age + "歳）を追加しました";
+//	}
+//
+//	@PatchMapping("/student")
+//	public void updateStudentName(String name, int age) {
+//		repository.updateStudent(name, age);
+//	}
+//
+//	@DeleteMapping("/student")
+//	public void deleteStudent(String name) {
+//		repository.deleteStudent(name);
+//	}
 }
